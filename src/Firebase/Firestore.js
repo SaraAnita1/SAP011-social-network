d20e5301c9d4e1c95db57779cbce8a67f2dffe22
JucieleGomes <jucielegomes.s@hotmail.com>
2023-09-27 14:37:18 -0300

    Separação de responsabilidades do firestore e dom iniciada

--- src/Firebase/Firestore.js
+++ src/Firebase/Firestore.js
@@ -17,13 +17,10 @@ export async function criarPublicacao(conteudoPublicacao) {
 }
 
 //Função que insere que busca o post no firebase e insere na tela
-export async function atualizarLinhaDoTempo(){
+export async function atualizarLinhaDoTempo(criarEstrturaDoPost, limparTela){
 const ordenar = query(collection(db, "publicacoes"), orderBy("data", "desc"))
-
+limparTela();
 let querySnapshot = await getDocs(ordenar);
-const conteudoLinhaDoTempo = document.querySelector("#conteudoLinhaDoTempo");
-conteudoLinhaDoTempo.innerHTML = "";
-
 querySnapshot.forEach((doc) => {
  const autor = doc.data().autor;
  const conteudo = doc.data().publicacao;
@@ -32,37 +29,7 @@ querySnapshot.forEach((doc) => {
  //const que guarda o valor do id de cada documento
  const idPublicacao = doc.id;
 
- //Criação dos elementos de post e icones de forma dinamica
- const iconeEditar = document.createElement("img")
- iconeEditar.src = "Imagens/editar.png";
- const iconeSalvar = document.createElement("img")
- iconeSalvar.src = "Imagens/IconeSalvar.png"
- const iconeLixeira = document.createElement("img")
- iconeLixeira.src = "Imagens/Lixeira.png"
- //o valor do documento é guardado em cada icone de lixeira
- // para quando houver o clique saber qual o id do documento clicacdo
- iconeLixeira.setAttribute ("data-postId", idPublicacao)
- const iconeCurtir = document.createElement("img")
- iconeCurtir.src = "Imagens/iconeCurtir.png"
- const iconeCurtida = document.createElement("img")
- iconeCurtida.src = "Imagens/iconeCurtida.png"
- 
-//inserindo os elementos criados no conteúdo da div de post criada no HTML
- const postagens = document.createElement("div");
- postagens.textContent = `${autor}: ${conteudo} ${data} ${curtidas}`;
- conteudoLinhaDoTempo.appendChild(postagens);
- conteudoLinhaDoTempo.appendChild(iconeEditar);
- conteudoLinhaDoTempo.appendChild(iconeSalvar);
- conteudoLinhaDoTempo.appendChild(iconeLixeira);
- conteudoLinhaDoTempo.appendChild(iconeCurtir);
- conteudoLinhaDoTempo.appendChild(iconeCurtida);
- 
- postagens.className = "postagens"
- iconeEditar.className = "editar"
- iconeSalvar.className = "salvar"
- iconeLixeira.className = "excluir"
- iconeCurtir.className = "curtir"
- iconeCurtida.className = "curtida"
+ criarEstrturaDoPost(autor, conteudo, data, curtidas, idPublicacao);
 
 });
 
@@ -82,4 +49,3 @@ export async function excluirPublicacao(idPublicacao){
   console.log("deletando post");
   await deleteDoc(doc(db, "publicacoes", idPublicacao));
 };
-
--- src/lib/LinhaDoTempo/linhaDoTempo.js
+++ src/lib/LinhaDoTempo/linhaDoTempo.js
@@ -52,7 +52,7 @@ export default () => {
       return alert("O campo de postagem não pode ser enviado vazio!")
     } else{
       criarPublicacao(conteudoPublicacao)
-      atualizarLinhaDoTempo();
+      atualizarLinhaDoTempo(criarEstrturaDoPost, limparTela);
     }
 
   });
@@ -74,7 +74,48 @@ function abrirCaixaDeTextoPostagem() {
 }
 
 //chama a função novamente para redenrizar os componentes na tela
-  atualizarLinhaDoTempo();
+atualizarLinhaDoTempo(criarEstrturaDoPost, limparTela);
+
+function limparTela(){
+const conteudoLinhaDoTempo = document.querySelector("#conteudoLinhaDoTempo");
+conteudoLinhaDoTempo.innerHTML = "";
+}
+
+function criarEstrturaDoPost(autor, conteudo, data, curtidas, idPublicacao){
+
+  //Criação dos elementos de post e icones de forma dinamica
+const iconeEditar = document.createElement("img")
+iconeEditar.src = "Imagens/editar.png";
+const iconeSalvar = document.createElement("img")
+iconeSalvar.src = "Imagens/IconeSalvar.png"
+const iconeLixeira = document.createElement("img")
+iconeLixeira.src = "Imagens/Lixeira.png"
+
+//o valor do documento é guardado em cada icone de lixeira
+// para quando houver o clique saber qual o id do documento clicacdo
+iconeLixeira.setAttribute ("data-postId", idPublicacao)
+const iconeCurtir = document.createElement("img")
+iconeCurtir.src = "Imagens/iconeCurtir.png"
+const iconeCurtida = document.createElement("img")
+iconeCurtida.src = "Imagens/iconeCurtida.png"
+
+//inserindo os elementos criados no conteúdo da div de post criada no HTML
+const postagens = document.createElement("div");
+postagens.textContent = `${autor}: ${conteudo} ${data} ${curtidas}`;
+conteudoLinhaDoTempo.appendChild(postagens);
+conteudoLinhaDoTempo.appendChild(iconeEditar);
+conteudoLinhaDoTempo.appendChild(iconeSalvar);
+conteudoLinhaDoTempo.appendChild(iconeLixeira);
+conteudoLinhaDoTempo.appendChild(iconeCurtir);
+conteudoLinhaDoTempo.appendChild(iconeCurtida);
+
+postagens.className = "postagens"
+iconeEditar.className = "editar"
+iconeSalvar.className = "salvar"
+iconeLixeira.className = "excluir"
+iconeCurtir.className = "curtir"
+iconeCurtida.className = "curtida"
+}
 
   return linhaDoTempo;
   
