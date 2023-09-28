927c9adfb32f18afe32fbfb0109427ec6d9f9a32
Sara Anita <sara.anita.desa@gmail.com>
2023-09-28 17:43:04 -0300

    att

--- src/lib/Cadastro/cadastro.js
+++ src/lib/Cadastro/cadastro.js
@@ -54,7 +54,7 @@ export default () => {
     event.preventDefault();
     const email = cadastro.querySelector('#email').value;
     const senha = cadastro.querySelector('#senha').value;
-    
+
     cadastrarUsuario(email, senha).then((response) => {
       window.location.hash = '#telaInicial';
       console.log('success', response);
